9f5199904a528d12537f8cb70aae81a7c9d1a499
Amlan Nath <amlannath95@gmail.com>
2021-07-01 00:27:46 +0530

    pubsec.yaml and build.gradle updated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// @dart=2.9
+
+import 'package:ai_radio/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +17,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: HomePage(),
     );
   }
 }
